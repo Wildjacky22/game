e0e96cc03969bea21a8b9beb0d3605f98f453e1f
Wildjacky22 <62379@bwls.net>
2016-10-14 08:33:08 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,13 @@
 function setup() {
   createCanvas( windowWidth , windowHeight );
+   fill( 0 , 0 , 0 , 1 );
+  textAlign( CENTER );
+  textSize( 60 );
+  text("KALEIDOSCOPE" , 0.5*xRes , 0.5*yRes );
+  textSize( 30 );
+  text( "Click or touch to draw.\n-marthematicist-" , 0.5*xRes , 0.5*yRes + 35 );
+  startTime = millis();
+}
 }
 
 function draw() {

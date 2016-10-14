7c72e9b09b957433f52aee55cd58125580423b5f
Wildjacky22 <62379@bwls.net>
2016-10-14 09:50:48 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,11 @@ function setup() {
 
 function draw() {
   ellipse( mouseX , mouseY , 50 , 50 );
+
+  // Define the fill color based off of the time variable.
+  // as time goes by, the color will change.
   fillcolor = color( t , 100 , 100 , markerAlpha );
-  
+
+  // Set the fill color
   fill( fillcolor );
 }

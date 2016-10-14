31f4a806e02274ed680fd59ea480558189d09b0e
Wildjacky22 <62379@bwls.net>
2016-10-14 11:40:58 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,10 +5,15 @@ function setup() {
 
 
 function draw() {
+  fill( 0 , 0 , 0 , 1 );
+  textAlign( CENTER );
+  textSize( 60 );
+  text("KALEIDOSCOPE" , 0.5*xRes , 0.5*yRes );
+
   ellipse( mouseX , mouseY , 50 , 50 );
 
   colorMode( HSB );
  
-  fill( 25 , 25 , 121 , markerAlpha );
+  fill( 25 , 25 , 121 , 200 );
 
 }

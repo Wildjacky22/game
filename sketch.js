37b94eada72c39b0d4daf67d5bceca7fd1acc0c4
Wildjacky22 <62379@bwls.net>
2016-10-14 13:14:58 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,5 @@
 function setup() {
   createCanvas( windowWidth , windowHeight );
-
-  setupGlobalVariables();
- 
-  xRes = windowWidth;
-  yRes = windowHeight;
- 
-  colorMode( HSB );
-
-  fill( 0 , 0 , 0 , 1 );
-  textAlign( CENTER );
-  textSize( 60 );
-  text("BUBBLES" , 0.5*xRes , 0.5*yRes );
-  
-  createCanvas( xRes , yRes );
- startTime = millis(); 
   
 }
 function draw() {

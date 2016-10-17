583408ff074393abffd08bc636fe82018b55b06e
Wildjacky22 <62379@bwls.net>
2016-10-17 09:45:57 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,11 +3,11 @@ function setup() {
   
 }
 function draw() {
+ ellipse( mouseX , mouseY , 75 , 75 );
+    fill( 25 , 25 , 255 )
+   
   ellipse( mouseX , mouseY , 50 , 50 );
     fill( 0 , 0 , 125 )
- 
-  ellipse( mouseX , mouseY , 75 , 75 );
-    fill( 0 , 0 , 255 )
   
   ellipse( mouseX , mouseY , 25 , 25 );
    fill( 25 , 25 , 121 , 200 );

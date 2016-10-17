90d2b6f21f6abe586139b81cb5375ce5cc871e4a
Wildjacky22 <62379@bwls.net>
2016-10-17 10:04:32 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,6 +6,15 @@ function draw() {
  ellipse( mouseX , mouseY , 75 , 75 );
     fill( 25 , 25 , 255 , 200 )
    
+  ellipse( mouseX , mouseY , 70 , 70 );
+    fill( 30 , 30 , 250 , 200 )
+    
+  ellipse( mouseX , mouseY , 65 , 65 );
+    fill( 35 , 35 , 249 , 200 )
+    
+  ellipse( mouseX , mouseY , 60 , 60 );
+    fill( 40 , 40 , 240 , 200 )
+    
   ellipse( mouseX , mouseY , 50 , 50 );
     fill( 65 , 230 , 240 , 200 )
   

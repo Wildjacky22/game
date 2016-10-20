1592fa59f538322911eb5a391a5645dd256bca35
Wildjacky22 <62379@bwls.net>
2016-10-20 11:56:57 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,45 +6,45 @@ function setup() {
 }
 function draw() {
  ellipse( mouseX , mouseY , 2 , 2 );
-    fill( 66 , 33 , 240 , 200);
+    fill( 66 , 33 , 240 , 100);
   
  ellipse( mouseX , mouseY , 75 , 75 );
-    fill( 66 , 44 , 240 , 200 );
+    fill( 66 , 44 , 240 , 100 );
    
   ellipse( mouseX , mouseY , 70 , 70 );
-    fill( 66 , 45 , 250 , 200 );
+    fill( 66 , 45 , 250 , 100 );
     
   ellipse( mouseX , mouseY , 65 , 65 );
-    fill( 66 , 49 , 249 , 200 );
+    fill( 66 , 49 , 249 , 100 );
     
   ellipse( mouseX , mouseY , 60 , 60 );
-    fill( 66 , 55 , 240 , 200 );
+    fill( 66 , 55 , 240 , 100 );
     
   ellipse( mouseX , mouseY , 55 , 55 );
-    fill( 66 , 66 , 244 , 200 );
+    fill( 66 , 66 , 244 , 100 );
     
   ellipse( mouseX , mouseY , 50 , 50 );
-    fill( 65 , 83 , 244 , 200 );
+    fill( 65 , 83 , 244 , 100 );
   
   ellipse( mouseX , mouseY , 45 , 45 );
-    fill( 66 , 116 , 244 , 200 );
+    fill( 66 , 116 , 244 , 100 );
     
   ellipse( mouseX , mouseY , 40 , 40 );
-    fill( 66 , 138 , 244 , 200 );
+    fill( 66 , 138 , 244 , 100 );
     
   ellipse( mouseX , mouseY , 35 , 35 );
-    fill( 66 , 153 , 255 , 200 );
+    fill( 66 , 153 , 255 , 100 );
     
   ellipse( mouseX , mouseY , 30 , 30 );
-    fill( 66 , 177 , 244 , 200 );
+    fill( 66 , 177 , 244 , 100 );
     
   ellipse( mouseX , mouseY , 25 , 25 );
-   fill( 66 , 192 , 244 , 200 );
+   fill( 66 , 192 , 244 , 100 );
   
   ellipse( mouseX , mouseY , 20 , 20 );
-   fill( 66 , 212 , 244 , 200);
+   fill( 66 , 212 , 244 , 100);
 
   ellipse( mouseX , mouseY , 15 , 15 );
-    fill( 66 , 233 , 255 , 200 );
+    fill( 66 , 233 , 255 , 100 );
     
 }
